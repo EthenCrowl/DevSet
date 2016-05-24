42b7a1d878f9505ab35fc78c091132f79396e59d
EthenCrowl <redwire500@gmail.com>
2016-05-23 23:20:14 -0700

    added requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.codeinput import CodeInput
 from kivy.extras.highlight import KivyLexer
-from kivy.extras.highlight import CythonLexer
+from pygments.lexers.python import Python3Lexer
+
+class Python3CodeInput(CodeInput):
+    def build(self):
+        return CodeInput(lexer=Python3Lexer())
 
 class SimpleKivy(App):
     def build(self):
